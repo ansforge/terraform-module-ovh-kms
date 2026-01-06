b60e10c7654fd40a950c69adf125b01f39a1a670
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2026-01-06 14:51:43 +0100

    feat: add variables for OVH KMS configuration

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "project_id" {
+  description = "OVH project id (optional)."
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "OVH region for the KMS resource ('eu-west-rbx')."
+  type        = string
+  default     = "eu-west-rbx"
+}
+
+variable "kms_key_name" {
+  description = "Name of the KMS key to create (informational)."
+  type        = string
+  default     = "ovh-kms-key"
+}
+
+variable "create_credential" {
+  description = "Whether to create a KMS credential for access."
+  type        = bool
+  default     = true
+}
+
+variable "certificate_type" {
+  description = "Type of certificate for the KMS credential."
+  default     = "ECDSA"
+}
+
+variable "credential_name" {
+  description = "Name of the KMS credential."
+  type        = string
+  default     = "kms-credential"
+}
+
+variable "credential_description" {
+  description = "Description of the KMS credential."
+  type        = string
+  default     = "KMS access credential"
+}
+
+variable "identity_urns" {
+  description = "List of identity URNs allowed to use this credential."
+  type        = list(string)
+  default     = []
+}
